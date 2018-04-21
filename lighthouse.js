f0d5e5f8e1a49bc421b5e569f8fac22046fbd6cb
Bryan Hunter <bdhunter3141@gmail.com>
2018-04-21 12:31:40 -0400

    completed challenge 14

--- lighthouse.js
+++ lighthouse.js
@@ -8,13 +8,13 @@ const GRID = [
   ["", "", "", "", "", "", "", "", "", ""],
   ["", "^", "~", "~", "", "", "", "^", "", ""],
   ["", "^", "", "~", "~", "", "", "", "", ""],
-  ["", "^", "", "", "~", "~", "", "", "", ""],
+  ["", "^", "", "", "~", "~", "", "", "", ""]
 ];
 
 function gridSize() {
   const y = GRID.length;
   const x = GRID[0].length;
-  return `${x} x ${y}`
+  return `${x} x ${y}`;
 }
 
 function totalCells() {
@@ -28,27 +28,49 @@ function lightCell(cell) {
   const posX = alphabet.indexOf(cell[0]);
   const posY = parseInt(cell.substring(1)) - 1;
   let cellExist = false;
-  if (posX < (GRID[0].length - 1) && posY < (GRID.length - 1)) {
+  if (posX < GRID[0].length - 1 && posY < GRID.length - 1) {
     cellExist = true;
   }
-  return (cellExist ? GRID[posY][posX] : cellExist);
+  return cellExist ? GRID[posY][posX] : cellExist;
 }
 
 function isRock(cell) {
   return lightCell(cell) === "^";
 }
 
+function containsRocks(cellArray) {
+  let rock = false;
+  for (cell of cellArray) {
+    if (cell === "^") {
+      rock = true;
+      break
+    }
+  }
+  return rock;
+}
+
 function isCurrent(cell) {
   return lightCell(cell) === "~";
 }
 
+function containsCurrents(cellArray) {
+  let current = false;
+  for (cell of cellArray) {
+    if (cell === "~") {
+      current = true;
+      break;
+    }
+  }
+  return current;
+}
+
 function lightRow(row) {
-	const rowToLight = GRID[row];
-	let litRow = [];
-	for (cell of rowToLight) {
-		litRow.push(cell);
-	}
-	return litRow;
+  const rowToLight = GRID[row];
+  let litRow = [];
+  for (cell of rowToLight) {
+    litRow.push(cell);
+  }
+  return litRow;
 }
 
 function lightColumn(column) {
@@ -75,7 +97,7 @@ function findCell(type) {
   for (row in GRID) {
     for (let i = 0; i < GRID[row].length; i++) {
       if (GRID[row][i] === type) {
-        cellArray.push(`${alphabet[i]}${parseInt(row) + 1}`)
+        cellArray.push(`${alphabet[i]}${parseInt(row) + 1}`);
       }
     }
   }
@@ -100,4 +122,22 @@ function firstCurrent() {
   return currentArray[0];
 }
 
-console.log(firstCurrent());
+function isDangerous(cell) {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const posX = alphabet.indexOf(cell[0]);
+  const posY = parseInt(cell.substring(1)) - 1;
+  const allCells = { 
+    originalCell: lightCell(cell), 
+    cellAbove: GRID[posY - 1][posX],
+    cellBelow: GRID[posY + 1][posX],
+    cellBefore: GRID[posY][posX - 1],
+    cellAfter: GRID[posY][posX + 1]
+   }
+   if (containsRocks([allCells.originalCell, allCells.cellAbove, allCells.cellBelow, allCells.cellBefore, allCells.cellAfter]) || containsCurrents([allCells.originalCell, allCells.cellAbove, allCells.cellBelow, allCells.cellBefore, allCells.cellAfter])) {
+     return true;
+   } else {
+     return false;
+   }
+}
+
+console.log(isDangerous("B9"), isDangerous("I6"), isDangerous("E4"));
